83421f0c9e140631c3fcfa2d9dce5fb91aa2c48d
Brian McMahon <30627595+cipher813@users.noreply.github.com>
2018-11-23 12:20:59 -0500

    fixed script charting error, drafted results notebook

--- scripts/base_dqn.py
+++ scripts/base_dqn.py
@@ -1,22 +1,25 @@
-"""Base DQN implementation."""
-
+"""
+Base DQN implementation.
+Adapted from code at https://github.com/udacity/deep-reinforcement-learning/tree/master/dqn
+"""
+# imports
 import re
+import pickle
 import random
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt
-
 from collections import namedtuple, deque
+from unityagents import UnityEnvironment
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from unityagents import UnityEnvironment
-
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+# path information
 PATH = "/Volumes/BC_Clutch/Dropbox/DeepRLND/rl_navigation/"
 APP_PATH = PATH + "data/Banana.app"
 CHART_PATH = PATH + "charts/"
@@ -26,7 +29,7 @@ BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate 
+LR = 5e-4               # learning rate
 UPDATE_EVERY = 4        # how often to update the network
 
 timestamp = re.sub(r"\D","",str(datetime.datetime.now()))[:12]
@@ -36,7 +39,7 @@ class Agent():
 
     def __init__(self, state_size, action_size, seed):
         """Initialize an Agent object.
-        
+
         Params
         ======
             state_size (int): dimension of each state
@@ -56,11 +59,11 @@ class Agent():
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
         # Initialize time step (for updating every UPDATE_EVERY steps)
         self.t_step = 0
-    
+
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory
         self.memory.add(state, action, reward, next_state, done)
-        
+
         # Learn every UPDATE_EVERY time steps.
         self.t_step = (self.t_step + 1) % UPDATE_EVERY
         if self.t_step == 0:
@@ -71,7 +74,7 @@ class Agent():
 
     def act(self, state, eps=0.):
         """Returns actions for given state as per current policy.
-        
+
         Params
         ======
             state (array_like): current state
@@ -94,14 +97,14 @@ class Agent():
 
         Params
         ======
-            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples
             gamma (float): discount factor
         """
         states, actions, rewards, next_states, dones = experiences
 
         # Get max predicted Q values (for next states) from target model
         Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
-        # Compute Q targets for current states 
+        # Compute Q targets for current states
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
         # Get expected Q values from local model
@@ -115,7 +118,7 @@ class Agent():
         self.optimizer.step()
 
         # ------------------- update target network ------------------- #
-        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)                     
+        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
 
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
@@ -125,7 +128,7 @@ class Agent():
         ======
             local_model (PyTorch model): weights will be copied from
             target_model (PyTorch model): weights will be copied to
-            tau (float): interpolation parameter 
+            tau (float): interpolation parameter
         """
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
@@ -145,16 +148,16 @@ class ReplayBuffer:
             seed (int): random seed
         """
         self.action_size = action_size
-        self.memory = deque(maxlen=buffer_size)  
+        self.memory = deque(maxlen=buffer_size)
         self.batch_size = batch_size
         self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
         self.seed = random.seed(seed)
-    
+
     def add(self, state, action, reward, next_state, done):
         """Add a new experience to memory."""
         e = self.experience(state, action, reward, next_state, done)
         self.memory.append(e)
-    
+
     def sample(self):
         """Randomly sample a batch of experiences from memory."""
         experiences = random.sample(self.memory, k=self.batch_size)
@@ -164,13 +167,13 @@ class ReplayBuffer:
         rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
         next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
         dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
-  
+
         return (states, actions, rewards, next_states, dones)
 
     def __len__(self):
         """Return the current size of internal memory."""
         return len(self.memory)
-    
+
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
@@ -195,10 +198,10 @@ class QNetwork(nn.Module):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
         return self.fc3(x)
-    
+
 def dqn(n_episodes=1000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995,train_mode=False):
     """Deep Q-Learning.
-    
+
     Params
     ======
         n_episodes (int): maximum number of training episodes
@@ -207,7 +210,7 @@ def dqn(n_episodes=1000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
         eps_end (float): minimum value of epsilon
         eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
     """
-    env_info = env.reset(train_mode=train_mode)[brain_name] 
+    env_info = env.reset(train_mode=train_mode)[brain_name]
     scores = []                        # list containing scores from each episode
     scores_window = deque(maxlen=100)  # last 100 scores
     eps = eps_start
@@ -218,11 +221,11 @@ def dqn(n_episodes=1000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
         score = 0
         for t in range(max_t):
             state = env_info.vector_observations[0]            # get the current state
-            action = agent.act(state, eps) 
+            action = agent.act(state, eps)
             env_info = env.step(action)[brain_name]        # send the action to the environment
             next_state = env_info.vector_observations[0]   # get the next state
             reward = env_info.rewards[0]                   # get the reward
-            done = env_info.local_done[0] 
+            done = env_info.local_done[0]
             agent.step(state, action, reward, next_state, done)
             score += reward                                # update the score
             state = next_state                             # roll over the state to next time step
@@ -242,6 +245,7 @@ def dqn(n_episodes=1000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
             break
     return scores
 
+# setup and examine environment
 env = UnityEnvironment(file_name=APP_PATH)
 
 brain_name = env.brain_names[0]
@@ -257,23 +261,19 @@ print('Number of agents:', len(env_info.agents))
 action_size = brain.vector_action_space_size
 print('Number of actions:', action_size)
 
-# examine the state space 
+# examine the state space
 state = env_info.vector_observations[0]
 print('States look like:\n', state)
 state_size = len(state)
 print('States have length:', state_size)
 
+# train agent
 agent = Agent(state_size=37, action_size=4, seed=0)
 
 scores = dqn(train_mode=True)
 
-# plot the scores
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.plot(np.arange(len(scores)), scores)
-plt.ylabel('Score')
-plt.xlabel('Episode #')
-chartpath = CHART_PATH + f"NavigationTrainChart-{timestamp}.jpg"
-plt.savefig(chartpath)
-print(f"Chart saved at {chartpath}")
-plt.show()
+pklpath = CHART_PATH + f"NavigationTrainChart-{timestamp}.pkl"
+
+with open(pklpath, 'wb') as handle:
+    pickle.dump(scores, handle)
+print(f"Scores pickled at {pklpath}")
