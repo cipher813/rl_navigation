3d40572c0d85f746e4fcce0c708efe03ccb4fcf4
Brian McMahon <30627595+cipher813@users.noreply.github.com>
2018-12-01 10:12:43 -0500

    updating unity function for VisualBanana capability

--- scripts/dqn_helper5.py
+++ scripts/dqn_helper5.py
@@ -377,6 +377,69 @@ class Dueling(PriorityReplay):
         x = val+adv-adv.mean(1).unsqueeze(1).expand(x.size(0),self.action_size)
         return x
 
+class A3C(Dueling):
+    """Inspired by code at https://github.com/ikostrikov/pytorch-a3c/blob/master/model.py."""
+    def __init__(self, state_size, action_size, seed):
+        super(ActorCritic, self).__init__(state_size, action_size, seed)
+        self.conv1 = nn.Conv2d(state_size, 32, 3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv3 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+        self.conv4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
+
+        self.lstm = nn.LSTMCell(32 * 3 * 3, 256)
+
+        self.action_size = action_size
+        self.critic_linear = nn.Linear(256, 1)
+        self.actor_linear = nn.Linear(256, action_size)
+
+        self.apply(weights_init)
+        self.actor_linear.weight.data = normalized_columns_initializer(
+            self.actor_linear.weight.data, 0.01)
+        self.actor_linear.bias.data.fill_(0)
+        self.critic_linear.weight.data = normalized_columns_initializer(
+            self.critic_linear.weight.data, 1.0)
+        self.critic_linear.bias.data.fill_(0)
+
+        self.lstm.bias_ih.data.fill_(0)
+        self.lstm.bias_hh.data.fill_(0)
+
+        self.train()
+
+    def forward(self, inputs):
+        inputs, (hx, cx) = inputs
+        x = F.elu(self.conv1(inputs))
+        x = F.elu(self.conv2(x))
+        x = F.elu(self.conv3(x))
+        x = F.elu(self.conv4(x))
+
+        x = x.view(-1, 32 * 3 * 3)
+        hx, cx = self.lstm(x, (hx, cx))
+        x = hx
+
+        return self.critic_linear(x), self.actor_linear(x), (hx, cx)
+
+    def normalized_columns_initializer(weights, std=1.0):
+        out = torch.randn(weights.size())
+        out *= std / torch.sqrt(out.pow(2).sum(1, keepdim=True))
+        return out
+
+    def weights_init(m):
+        classname = m.__class__.__name__
+        if classname.find('Conv') != -1:
+            weight_shape = list(m.weight.data.size())
+            fan_in = np.prod(weight_shape[1:4])
+            fan_out = np.prod(weight_shape[2:4]) * weight_shape[0]
+            w_bound = np.sqrt(6. / (fan_in + fan_out))
+            m.weight.data.uniform_(-w_bound, w_bound)
+            m.bias.data.fill_(0)
+        elif classname.find('Linear') != -1:
+            weight_shape = list(m.weight.data.size())
+            fan_in = weight_shape[1]
+            fan_out = weight_shape[0]
+            w_bound = np.sqrt(6. / (fan_in + fan_out))
+            m.weight.data.uniform_(-w_bound, w_bound)
+            m.bias.data.fill_(0)
+
 def train_gym(CHART_PATH, CHECKPOINT_PATH, agent_dict, module, timestamp, seed, score_target,
               n_episodes,max_t,eps_start,eps_end,eps_decay): #agent_dict,
     """Trains OpenAI Gym environments."""
@@ -386,12 +449,6 @@ def train_gym(CHART_PATH, CHECKPOINT_PATH, agent_dict, module, timestamp, seed, 
     env = gym.make(module)
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
-#    agent_dict = {
-#              "Dueling":Dueling(state_size,action_size,seed),
-#              "PriorityReplay":PriorityReplay(state_size, action_size, seed),
-#              "Double":Double(state_size, action_size, seed),
-#              "Vanilla":Vanilla(state_size, action_size, seed)
-#             }
     result_dict = {}
     for k,v in agent_dict.items():
         agent_name = k
@@ -435,10 +492,11 @@ def train_gym(CHART_PATH, CHECKPOINT_PATH, agent_dict, module, timestamp, seed, 
         print(f"Scores pickled at {pklpath}")
     return result_dict
 
-def train_unity(APP_PATH, CHART_PATH, CHECKPOINT_PATH, agent_dict, timestamp, seed, score_target,
+def train_unity(CHART_PATH, CHECKPOINT_PATH, agent_dict, module, timestamp, seed, score_target,
                 n_episodes,max_t,eps_start,eps_end,eps_decay):
     """Trains Unity 3D Editor environments."""
     from unityagents import UnityEnvironment
+    APP_PATH = PATH + f"data/{module.split('_')[0]}" # select app whether or not suffice is "_Linux"
     label = "unity"
     start = time.time()
     env = UnityEnvironment(file_name=APP_PATH)
@@ -447,12 +505,6 @@ def train_unity(APP_PATH, CHART_PATH, CHECKPOINT_PATH, agent_dict, timestamp, se
     env_info = env.reset()[brain_name]
     state_size = len(env_info.vector_observations[0])
     action_size = brain.vector_action_space_size
-#    agent_dict = {
-#              "Dueling":Dueling(state_size,action_size,seed),
-#              "PriorityReplay":PriorityReplay(state_size, action_size, seed),
-#              "Double":Double(state_size, action_size, seed),
-#              "Vanilla":Vanilla(state_size, action_size, seed)
-#             }
     result_dict = {}
     for k,v in agent_dict.items():
         agent_name = k
@@ -541,19 +593,12 @@ def train_envs(PATH, CHART_PATH, CHECKPOINT_PATH, agent_dict, timestamp, env_dic
         platform = v[0]
         print(f"Begin training {module}-{platform}.")
         score_target = v[1]
-        # seed = 0
-        # n_episodes=3000
-        # max_t=1000
-        # eps_start=0.4
-        # eps_end=0.01
-        # eps_decay=0.995
         print(f"Module: {module}-{platform}")
         if platform == "gym":
             results = train_gym(CHART_PATH, CHECKPOINT_PATH, agent_dict, module, timestamp, seed, score_target,
                             n_episodes,max_t,eps_start,eps_end,eps_decay)
         elif platform == "unity":
-            APP_PATH = PATH + "data/Banana.app"
-            results = train_unity(APP_PATH, CHART_PATH, CHECKPOINT_PATH, agent_dict, timestamp, seed, score_target,
+            results = train_unity(CHART_PATH, CHECKPOINT_PATH, agent_dict, module, timestamp, seed, score_target,
                           n_episodes,max_t,eps_start,eps_end,eps_decay)
         else:
             print("Check your model and platform inputs.")
