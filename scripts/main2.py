de07a217a1b2446a102380e57299e3f9dff3f92a
Brian McMahon <30627595+cipher813@users.noreply.github.com>
2018-12-01 11:37:59 -0500

    updated script to match latest notebook

--- scripts/main2.py
+++ scripts/main2.py
@@ -1,10 +1,11 @@
-from dqn_helper4 import *
+from dqn_helper5 import *
 
 import re
 import datetime
 
 # path information
-PATH = "/Volumes/BC_Clutch/Dropbox/DeepRLND/rl_navigation/"
+PATH = "/Volumes/BC_Clutch/Dropbox/DeepRLND/rl_navigation/" # mac
+# PATH = "/home/brianmcmahon/rl_navigation/" # google cloud
 CHART_PATH = PATH + "charts/"
 CHECKPOINT_PATH = PATH + "models/"
 
@@ -19,11 +20,27 @@ eps_decay=0.995
 
 rd = {}
 env_dict = {
-#            "Bananas":["unity",13.0],
-            "LunarLander-v2":["gym",200.0],
-#            "CartPole-v0":["gym",195.0],
-#            "MountainCar-v0":["gym",-110],
+            # unity, for google cloud
+            # "Banana_Linux_NoVis/Banana.x86_64":["unity",13.0],
+            # "VisualBanana_Linux_NoVis/Banana.x86_64":["unity",13.0],
+
+            # unity, for mac
+            "Banana.app":["unity",13.0],
+            "VisualBanana.app":["unity",13.0],
+
+            # OpenAI Gym
+#             "LunarLander-v2":["gym",200.0],
+#             "CartPole-v0":["gym",195.0],
+#             "MountainCar-v0":["gym",-110],
             }
 
-rd = train_envs(PATH, CHART_PATH, CHECKPOINT_PATH, timestamp, env_dict, seed,
+agent_dict = {
+              "A3C":A3C,
+              "Dueling":Dueling,
+              "PriorityReplay":PriorityReplay,
+              "Double":Double,
+              "Vanilla":Vanilla
+             }
+
+rd = train_envs(PATH, CHART_PATH, CHECKPOINT_PATH, agent_dict, timestamp, env_dict, seed,
                 n_episodes,max_t,eps_start,eps_end,eps_decay)
